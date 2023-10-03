15293da12df5f0db9944ed7cdbc404a3c6a051e1
VishalJadhavQA <vishaljadhav.career@gmail.com>
2023-10-03 19:03:49 +0530

    Selenium basics

---
+++ LearnSelenium/src/test/java/Selenium/Webdriver/_06_Waiting_mechanism.java
@@ -0,0 +1,52 @@
+package Selenium.Webdriver;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class _06_Waiting_mechanism {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		// Implicit Wait or Global wait - wait for all the web elements 
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("http://omayo.blogspot.com/");
+		driver.findElement(By.className("dropbtn")).click();
+		
+		// Thread.sleep() - Java wait
+		Thread.sleep(10000);     
+		driver.findElement(By.linkText("Facebook")).click();
+		
+		
+		driver.navigate().back();
+		
+		
+		
+		driver.findElement(By.className("dropbtn")).click();
+		
+		// Explicit Wait - wait only for the specific web element
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+		
+		/*wait till Gmail is visible*/
+		WebElement gmail = wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Gmail")));
+		
+		gmail.click();
+		
+		
+		
+	}
+
+}
