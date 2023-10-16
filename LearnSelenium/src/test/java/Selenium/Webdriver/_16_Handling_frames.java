1ede77a8dded2b6211480cfa44ccdf39ab0e986f
VishalJadhavQA <vishaljadhav.career@gmail.com>
2023-10-16 15:57:35 +0530

    Selenium basics

--- LearnSelenium/src/test/java/Selenium/Webdriver/_16_Handling_frames.java
+++ LearnSelenium/src/test/java/Selenium/Webdriver/_16_Handling_frames.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -20,7 +21,7 @@ public class _16_Handling_frames {
 		//Handling iframe
 		driver.get("https://the-internet.herokuapp.com/iframe");
 		
-		driver.switchTo().frame("mce_0_ifr");
+		driver.switchTo().frame("mce_0_ifr");  //using frame id 
 		driver.findElement(By.id("tinymce")).clear();
 		driver.findElement(By.id("tinymce")).sendKeys("Switched to iframe sucessfully.");
 		
@@ -29,10 +30,16 @@ public class _16_Handling_frames {
 		driver.switchTo().newWindow(WindowType.TAB);
 		
 		driver.get("https://docs.oracle.com/javase/8/docs/api/");
-		driver.switchTo().frame("classFrame");
+		driver.switchTo().frame("classFrame");           //using frame name
 		driver.findElement(By.linkText("Description")).click();
 		
+		//Handling frame
+		driver.switchTo().newWindow(WindowType.TAB);
+		driver.get("https://blogpendingtasks.blogspot.com/p/switchtoframeusingwebelement.html");
 		
+		WebElement frame = driver.findElement(By.xpath("//iframe[@title='arunmotoori']"));
+		driver.switchTo().frame(frame); //using webelement
+		driver.findElement(By.linkText("Home")).click();
 		
 		
 		
---
+++ LearnSelenium/src/test/java/Selenium/Webdriver/_17_Nested_frames.java
@@ -0,0 +1,66 @@
+package Selenium.Webdriver;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class _17_Nested_frames {
+
+	public static void main(String[] args) {
+		
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://letcode.in/frame");
+		
+		driver.switchTo().frame("firstFr");
+		driver.findElement(By.name("fname")).sendKeys("Test");
+		driver.findElement(By.name("lname")).sendKeys("Progress");
+		
+		WebElement frame = driver.findElement(By.xpath("//iframe[@class='has-background-white']"));
+		driver.switchTo().frame(frame); //swithcing to nested frame
+				
+		driver.findElement(By.className("input")).sendKeys("Test@mail.com");
+		
+		
+		driver.switchTo().newWindow(WindowType.TAB);
+		driver.get("https://the-internet.herokuapp.com/nested_frames");
+		
+		//Switch to parent frame - Top
+		driver.switchTo().frame("frame-top");
+		
+		//Switch to child frame - left
+		driver.switchTo().frame("frame-left");
+		
+		String frame1 = driver.findElement(By.xpath("//body")).getText();
+		System.out.println(frame1);
+		
+		//Switch again to parent frame - Top
+		driver.switchTo().parentFrame();
+		
+		//Switch to child frame - middle
+		driver.switchTo().frame("frame-middle");
+		
+		String frame2 = driver.findElement(By.id("content")).getText();
+		System.out.println(frame2);
+		
+		
+		//Switch back to page level or select first frame on the page
+		driver.switchTo().defaultContent();
+		
+		driver.switchTo().frame("frame-bottom");
+		String frame3 = driver.findElement(By.xpath("//body")).getText();
+		
+		System.out.println(frame3);
+		
+	}
+
+}
