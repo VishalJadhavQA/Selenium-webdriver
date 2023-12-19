48a9d4224c18a52886e7f57aa640ef21ee013502
VishalJadhavQA <vishaljadhav.career@gmail.com>
2023-12-19 19:22:00 +0530

    Selenium basics

---
+++ LearnSelenium/src/test/java/Selenium/Webdriver/_24_Static_table.java
@@ -0,0 +1,47 @@
+package Selenium.Webdriver;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class _24_Static_table {
+
+	public static void main(String[] args) {
+
+		
+			WebDriverManager.chromedriver().setup();
+			WebDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			
+			driver.get("https://omayo.blogspot.com/");	
+			
+			
+			/* Listing the table heading */ 
+			
+			List<WebElement> Heading = driver.findElements(By.xpath("//table[@id='table1']//th"));
+		
+			for (WebElement THeading : Heading) {
+				
+				System.out.println(THeading.getText());
+				
+			}
+		
+			/* Listing the table data */ 
+			
+			List<WebElement> Data = driver.findElements(By.xpath("//table[@id='table1']//td"));
+			
+			for (WebElement TData : Data) {
+				
+				System.out.print(TData.getText()+ " ");
+				
+			}
+
+	}
+
+}
