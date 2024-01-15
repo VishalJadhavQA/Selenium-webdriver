c37d0d0d241cf8d06d01cdeb08747305a762bc8f
VishalJadhavQA <vishaljadhav.career@gmail.com>
2024-01-15 20:15:30 +0530

    Selenium basics

---
+++ LearnSelenium/src/test/java/Selenium/Webdriver/_25_Apache_POI.java
@@ -0,0 +1,25 @@
+package Selenium.Webdriver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import com.google.common.escape.Escaper;
+
+public class _25_Apache_POI {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		
+		 String excelfilepath = System.getProperty("user.dir")+ "\\Files\\employees.xlsx";
+		 
+		 File excelfile = new File(excelfilepath);
+		 
+		 FileInputStream fis = new FileInputStream(excelfile);
+		 
+		 
+		 
+
+	}
+
+}
